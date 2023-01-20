00cde5e24170f309bd9cee55627f514edb2ca8e7
Matias Villar <95631527+matvillar@users.noreply.github.com>
2023-01-19 20:28:53 -0700

    updates

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const env = require('dotenv').config();
+const app = express();
 
 /* ***********************
  * Routes
  *************************/
-app.use(require("./routes/static"))
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', './layouts/layout'); // not at views root
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(require('./routes/static'));
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
